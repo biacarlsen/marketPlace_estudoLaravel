1f5f72d8dc3b30300b0371da83cb371342944dee
Bianca Carlsen <bia.carlsen@gmail.com>
2020-05-02 21:46:32 -0300

    Estrutura do projeto + migrations e seed users

---
+++ database/migrations/2020_05_02_224855_create_table_store.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTableStore extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stores', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+
+            $table->string('name');
+            $table->string('description');
+            $table->string('phone');
+            $table->string('mobile_phone');
+            $table->string('slug');
+
+            $table->timestamps();
+
+            // referenciando informações entre tabelas (stores e users)
+            $table->foreign('user_id')->references('id')->on('users');
+            // nome da chave estrangeira: stores_user_id_foreign (nome da tabela_nome da coluna)
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('stores');
+    }
+}
